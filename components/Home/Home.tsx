4720ab40280a324c99b96de8f046970739a7ee26
debora <debora.r.serra@gmail.com>
2023-01-02 17:13:14 -0300

    feat: add home page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import { useWindowSize } from "usehooks-ts";
 import Profile from "../../assets/images/home/profile.jpg";
 
 import * as S from "./Home.styled";
@@ -7,33 +8,37 @@ import * as S from "./Home.styled";
 interface Props {}
 
 const Home: NextPage<Props> = () => {
+  const { width } = useWindowSize();
   return (
     <S.Main>
-      <h1 className="home__title">Hello world!</h1>
-      <h2 className="home__subtitle">My name is Débora Serra</h2>
-      <Image
-        src={Profile}
-        alt="Débora Serra"
-        width={500}
-        height={600}
-        className="home__img"
-      />
-      <p className="home__description">
-        Web developer student in Trybe | Masters degree in Chemistry by UnB
-      </p>
-      <p className="home__description">
-        Cosmetologist by iCosmetologia | Post-graduated in Gastronomy in PUC-RS
-      </p>
-      <p className="home__description">
-        See my{" "}
-        <a
-          href="https://github.com/DeboraSerra"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1 className="home__title">Hello world!</h1>
+        <h2 className="home__subtitle">I am Débora Serra</h2>
+        <Image
+          src={Profile}
+          alt="Débora Serra"
+          width={500}
+          height={600}
+          className="home__img"
+        />
+        <p className="home__description">
+          Full stack web developer by Trybe {width > 576 ? '|' : <br />} Masters degree in Chemistry by UnB
+        </p>
+        <p className="home__description">
+          Cosmetologist by iCosmetologia {width > 576 ? '|' : <br />} Post-graduated in Gastronomy in
+          PUC-RS
+        </p>
+        <p className="home__description">
+          See my{" "}
+          <a
+            href="https://github.com/DeboraSerra"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
     </S.Main>
   );
 };
