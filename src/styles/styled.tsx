4c663131b358899d16dabf69478e24355d78cae0
Debora Serra <debora.r.serra@gmail.com>
2023-05-16 07:16:21 -0300

    feat: add babel

---
+++ src/styles/styled.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+export const Title = styled.h1``;
+
+export const Subtitle = styled.h2``;
+
+export const Text = styled.p``;
+
+export const ButtonLink = styled.a``;
+
+export const Button = styled.button``;
+
+export const LinkStyled = styled(Link)``;
+
+export const ModalCard = styled.div``;
