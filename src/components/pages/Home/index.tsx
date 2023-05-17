4df2f5bd20b0be529741db33c19d7c5b045eacf3
Debora Serra <debora.r.serra@gmail.com>
2023-05-17 07:31:23 -0300

    trying to make styled components work

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,19 +5,21 @@ import profile from "../../../../assets/images/home/me.jpg";
 import Image from "next/image";
 
 const Home = () => {
-  return <S.Main>
-    <div className="container">
-      <Title className="center">Hi! I&apos;m </Title>
-      <div className="home__intro flex center">
-        <Image src={profile} alt="Débora Serra" width={3880} height={5184} />
-        <div className="home__intro--right">
-          <Subtitle>Débora Serra</Subtitle>
-          <Text>Full Stack Web Developer | Chemist</Text>
-          <Text>Baker | Cosmetologist</Text>
+  return (
+    <S.Main>
+      <div className='container'>
+        <Title className='center'>Welcome to my Portfolio!</Title>
+        <div className='home__intro flex center'>
+          <Image src={profile} alt='Débora Serra' width={388.0} height={518.4} />
+          <div className='home__intro--right'>
+            <Subtitle>Débora Serra</Subtitle>
+            <Text>Full Stack Web Developer | Chemist</Text>
+            <Text>Baker | Cosmetologist</Text>
+          </div>
         </div>
       </div>
-    </div>
-  </S.Main>
-}
+    </S.Main>
+  );
+};
 
-export default Home
+export default Home;
