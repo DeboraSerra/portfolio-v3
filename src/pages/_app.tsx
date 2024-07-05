20867a9a835514e01dd2ae2408b7293c48782d7c
Débora Serra <debora.r.serra@gmail.com>
2024-07-05 11:46:43 -0300

    chore: add metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import TProvider from "@/styles/ThemeProvider";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 interface Props extends AppProps {}
@@ -36,6 +37,12 @@ export default function App({ Component, pageProps }: Props) {
   return (
     <TProvider isDarkMode={dark}>
       <ProjectsProvider>
+        <Head>
+          <title>Portfolio - Débora Serra</title>
+          <meta property='og:image' content="/profile.jpg" />
+          <meta property="og:image:alt" content="Portfolio - Débora Serra" />
+          <meta property="og:title" content="Portfolio - Débora Serra" />
+        </Head>
         <Header isDarkMode={dark} toggleDarkMode={toggleDarkMode} />
         <Component {...pageProps} />
         <Footer />
